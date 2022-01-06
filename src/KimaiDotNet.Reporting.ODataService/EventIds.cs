4cd5ec654ef9a7e5b6640e0b67fe433803fe7f97
MarkZither <mark.burton@zither-it.co.uk>
2022-01-06 21:49:40 +0100

    Add EventIDs class

---
+++ src/KimaiDotNet.Reporting.ODataService/EventIds.cs
@@ -0,0 +1,7 @@
+﻿namespace MarkZither.KimaiDotNet.Reporting.ODataService
+{
+    public static class EventIds{
+        public static readonly EventId ReadUserCacheError = new EventId(1, "Could not read User cache");
+        public static readonly EventId WriteUserCacheError = new EventId(2, "Could not write User cache");
+    }
+}
